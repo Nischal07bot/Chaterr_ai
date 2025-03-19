d7ed97f87f66dd1eb680df425f0a5d2f7e31d362
Nischal07bot <singhnischal953@gmail.com>
2025-03-19 13:51:46 +0000

    fixed a import bug

--- backend/controller/project.controller.js
+++ backend/controller/project.controller.js
@@ -1,5 +1,5 @@
 import projectModel from '../models/project.model.js';
-import * as projectService from '../services/project.service.js';
+import * as projectService from '../Services/project.service.js';
 import { validationResult } from 'express-validator';
 import userModel from '../models/user.model.js';
 export const createProject = async (req, res) => {
